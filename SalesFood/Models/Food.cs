043e813fe3c6fcc5d4eec35b3525c7ebbfffedcb
abraojr <abraoastjr@gmail.com>
2025-01-21 12:50:46 -0300

    feat: Creating the domain model

---
+++ SalesFood/Models/Food.cs
@@ -0,0 +1,18 @@
+﻿namespace SalesFood.Models;
+
+public class Food
+{
+    public int FoodId { get; set; }
+    public string Name { get; set; }
+    public string ShortDescription { get; set; }
+    public string LongDescription { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; }
+    public string ImageThumbnailUrl { get; set; }
+    public bool IsFavoriteFood { get; set; }
+    public bool InStock { get; set; }
+
+    public int CategoryId { get; set; }
+    public virtual Category Category { get; set; }
+}
+
