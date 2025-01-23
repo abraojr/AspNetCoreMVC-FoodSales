2207860769b1452053b516251fef1bb231d6f134
abraojr <abraoastjr@gmail.com>
2025-01-23 10:03:15 -0300

    feat: Creating a controller and a view to display a food list

---
+++ SalesFood/Controllers/FoodController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesFood.Repositories.Interfaces;
+
+namespace SalesFood.Controllers;
+
+public class FoodController : Controller
+{
+    private readonly IFoodRepository _foodRepository;
+
+    public FoodController(IFoodRepository foodRepository)
+    {
+        _foodRepository = foodRepository;
+    }
+
+    public IActionResult List()
+    {
+        var foods = _foodRepository.Foods;
+        return View(foods);
+    }
+}
+
