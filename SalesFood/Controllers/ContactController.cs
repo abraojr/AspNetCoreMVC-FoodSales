f9d9640c7214c666c7d94853252eae14984c4d05
abraojr <abraoastjr@gmail.com>
2025-01-28 13:48:11 -0300

    feat: Creating a TagHelper to send an e-mail on the Contact view

---
+++ SalesFood/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesFood.Controllers;
+
+public class ContactController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+
