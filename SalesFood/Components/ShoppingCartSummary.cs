fd754f4f4a00be206509c22f75fd3e505663fdbe
abraojr <abraoastjr@gmail.com>
2025-01-28 12:19:03 -0300

    feat: Creating a ViewComponent to display shopping cart details

---
+++ SalesFood/Components/ShoppingCartSummary.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesFood.Models;
+using SalesFood.ViewModels;
+
+namespace SalesFood.Components;
+
+public class ShoppingCartSummary : ViewComponent
+{
+    private readonly ShoppingCart _shoppingCart;
+
+    public ShoppingCartSummary(ShoppingCart shoppingCart)
+    {
+        _shoppingCart = shoppingCart;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var items = _shoppingCart.GetShoppingCartItems();
+
+        _shoppingCart.ShoppingCartItems = items;
+
+        var shoppingCartViewModel = new ShoppingCartViewModel
+        {
+            ShoppingCart = _shoppingCart,
+            ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+        };
+
+        return View(shoppingCartViewModel);
+    }
+}
+
