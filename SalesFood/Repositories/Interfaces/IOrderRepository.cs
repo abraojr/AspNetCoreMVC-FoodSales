ba94117757aa8f34d1074f4560f02f872d1f5b1d
abraojr <abraoastjr@gmail.com>
2025-01-29 16:46:46 -0300

    feat: Creating order repository

---
+++ SalesFood/Repositories/Interfaces/IOrderRepository.cs
@@ -0,0 +1,7 @@
+﻿using SalesFood.Models;
+
+namespace SalesFood.Repositories.Interfaces;
+public interface IOrderRepository
+{
+    void CreateOrder(Order order);
+}
---
+++ SalesFood/Repositories/OrderRepository.cs
@@ -0,0 +1,41 @@
+﻿using SalesFood.Context;
+using SalesFood.Models;
+using SalesFood.Repositories.Interfaces;
+
+namespace SalesFood.Repositories;
+
+public class OrderRepository : IOrderRepository
+{
+    private readonly AppDbContext _appDbContext;
+    private readonly ShoppingCart _shoppingCart;
+
+    public OrderRepository(AppDbContext appDbContext, ShoppingCart shoppingCart)
+    {
+        _appDbContext = appDbContext;
+        _shoppingCart = shoppingCart;
+    }
+
+    public void CreateOrder(Order order)
+    {
+        order.OrderDate = DateTime.Now;
+        _appDbContext.Orders.Add(order);
+        _appDbContext.SaveChanges();
+
+        var shoppingCartItems = _shoppingCart.ShoppingCartItems;
+
+        foreach (var cartItem in shoppingCartItems)
+        {
+            var orderDetail = new OrderDetail
+            {
+                Quantity = cartItem.Quantity,
+                FoodId = cartItem.Food.FoodId,
+                OrderId = order.OrderId,
+                Price = cartItem.Food.Price
+            };
+
+            _appDbContext.OrderDetails.Add(orderDetail);
+        }
+
+        _appDbContext.SaveChanges();
+    }
+}
