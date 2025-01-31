7a2dc284ba7179f20865015fb034aaa16731c125
abraojr <abraoastjr@gmail.com>
2025-01-31 14:28:54 -0300

    feat: Created the Action Checkout method and the CompleteCheckout view

--- SalesFood/Controllers/OrderController.cs
+++ SalesFood/Controllers/OrderController.cs
@@ -25,6 +25,47 @@ public class OrderController : Controller
     [HttpPost]
     public IActionResult Checkout(Order order)
     {
-        return View();
+        int totalOrderitems = 0;
+        decimal totalPrice = 0.0m;
+
+        // Gets the items from the customer's shopping cart
+        List<ShoppingCartItem> items = _shoppingCart.GetShoppingCartItems();
+        _shoppingCart.ShoppingCartItems = items;
+
+        // Checks for order items
+        if (_shoppingCart.ShoppingCartItems.Count == 0)
+        {
+            ModelState.AddModelError("", "Your shopping cart is empty");
+        }
+
+        // Calculate the total number of items and the total price of the order
+        foreach (var item in items)
+        {
+            totalOrderitems += item.Quantity;
+            totalPrice += item.Food.Price * item.Quantity;
+        }
+
+        // Set the values obtained to the order
+        order.TotalOrderItems = totalOrderitems;
+        order.TotalOrder = totalPrice;
+
+        // Validate the order data
+        if (ModelState.IsValid)
+        {
+            // Create the order and details
+            _orderRepository.CreateOrder(order);
+
+            // Set messages to the client
+            ViewBag.CompleteCheckoutMessage = "Thanks for your order :)";
+            ViewBag.TotalOrder = _shoppingCart.GetShoppingCartTotal();
+
+            // Clear the customer's shopping cart
+            _shoppingCart.ClearShoppinCart();
+
+            // Displays the view with customer and order data
+            return View("~/Views/Order/CompleteCheckout.cshtml", order);
+        }
+
+        return View(order);
     }
 }
