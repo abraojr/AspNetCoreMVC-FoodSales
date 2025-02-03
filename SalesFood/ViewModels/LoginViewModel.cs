7546534309e4848d1036075eb06fd006f5b395f6
abraojr <abraoastjr@gmail.com>
2025-02-03 19:03:32 -0300

    feat: Implementing login

---
+++ SalesFood/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesFood.ViewModels;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "Name is required")]
+    [Display(Name = "User")]
+    public string UserName { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+
+    public string ReturnUrl { get; set; }
+}
