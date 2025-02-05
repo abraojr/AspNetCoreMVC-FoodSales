d9b63a14cdc4e5ff9eb05c26dd5220a76cd9a350
abraojr <abraoastjr@gmail.com>
2025-02-05 17:34:31 -0300

    feat: Creating the AccessDenied view and menu for the Admin user

--- SalesFood/Controllers/AccountController.cs
+++ SalesFood/Controllers/AccountController.cs
@@ -93,4 +93,9 @@ public class AccountController : Controller
         await _signInManager.SignOutAsync();
         return RedirectToAction("Index", "Home");
     }
+
+    public IActionResult AccessDenied()
+    {
+        return View();
+    }
 }
