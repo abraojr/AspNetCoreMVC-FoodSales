b63f6e40ee66f79cba02367a9c1b2869efef84cc
abraojr <abraoastjr@gmail.com>
2025-02-10 14:03:05 -0300

    feat: Creating controllers and views to manage food, categories and orders

---
+++ SalesFood/Areas/Admin/Controllers/AdminFoodsController.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalesFood.Context;
+using Microsoft.AspNetCore.Authorization;
+using SalesFood.Models;
+
+namespace SalesFood.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = "Admin")]
+public class AdminFoodsController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public AdminFoodsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: Admin/AdminFoods
+    public async Task<IActionResult> Index()
+    {
+        var appDbContext = _context.Foods.Include(l => l.Category);
+        return View(await appDbContext.ToListAsync());
+    }
+
+    // GET: Admin/AdminFoods/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var food = await _context.Foods
+            .Include(l => l.Category)
+            .FirstOrDefaultAsync(m => m.FoodId == id);
+
+        if (food == null)
+        {
+            return NotFound();
+        }
+
+        return View(food);
+    }
+
+    // GET: Admin/AdminFoods/Create
+    public IActionResult Create()
+    {
+        ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name");
+        return View();
+    }
+
+    // POST: Admin/AdminFoods/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("FoodId,Name,ShortDescription,LongDescription,Price,ImageUrl,ImageThumbnailUrl,IsFavoriteFood,InStock,CategoryId")] Food food)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(food);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name", food.CategoryId);
+        return View(food);
+    }
+
+    // GET: Admin/AdminFoods/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var food = await _context.Foods.FindAsync(id);
+
+        if (food == null)
+        {
+            return NotFound();
+        }
+
+        ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name", food.CategoryId);
+        return View(food);
+    }
+
+    // POST: Admin/AdminFoods/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("FoodId,Name,ShortDescription,LongDescription,Price,ImageUrl,ImageThumbnailUrl,IsFavoriteFood,InStock,CategoryId")] Food food)
+    {
+        if (id != food.FoodId)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(food);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FoodExists(food.FoodId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name", food.CategoryId);
+        return View(food);
+    }
+
+    // GET: Admin/AdminFoods/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var food = await _context.Foods
+            .Include(l => l.Category)
+            .FirstOrDefaultAsync(m => m.FoodId == id);
+
+        if (food == null)
+        {
+            return NotFound();
+        }
+
+        return View(food);
+    }
+
+    // POST: Admin/AdminFoods/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var food = await _context.Foods.FindAsync(id);
+
+        _context.Foods.Remove(food);
+
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction("Index");
+    }
+
+    private bool FoodExists(int id)
+    {
+        return _context.Foods.Any(e => e.FoodId == id);
+    }
+}
---
+++ SalesFood/Areas/Admin/Controllers/AdminOrdersController.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalesFood.Context;
+using SalesFood.Models;
+
+namespace SalesFood.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminOrdersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AdminOrdersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/AdminOrders
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Orders.ToListAsync());
+        }
+
+        // GET: Admin/AdminOrders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // GET: Admin/AdminOrders/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/AdminOrders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("OrderId,Name,LastName,Address,AddressComplement,ZipCode,State,City,PhoneNumber,Email,OrderDate,DeliveryDate")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(order);
+        }
+
+        // GET: Admin/AdminOrders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FindAsync(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Admin/AdminOrders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("OrderId,Name,LastName,Address,AddressComplement,ZipCode,State,City,PhoneNumber,Email,OrderDate,DeliveryDate")] Order order)
+        {
+            if (id != order.OrderId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.OrderId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(order);
+        }
+
+        // GET: Admin/AdminOrders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // POST: Admin/AdminOrders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+
+            _context.Orders.Remove(order);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.OrderId == id);
+        }
+    }
+}
