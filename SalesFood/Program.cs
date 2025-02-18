f028c220fe39884c14fb11cd90f8c1ea1bdb860d
abraojr <abraoastjr@gmail.com>
2025-02-18 14:51:12 -0300

    feat: Migrating the code from the Startup class to the Program class

--- SalesFood/Program.cs
+++ SalesFood/Program.cs
@@ -1,17 +1,104 @@
-namespace SalesFood;
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args)
-           .Build()
-           .Run();
-    }
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SalesFood.Areas.Admin.Services;
+using SalesFood.Context;
+using SalesFood.Models;
+using SalesFood.Repositories.Interfaces;
+using SalesFood.Repositories;
+using SalesFood.Services;
+using ReflectionIT.Mvc.Paging;
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+var builder = WebApplication.CreateBuilder(args);
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+var imageFolderConfig = builder.Configuration.GetSection("ConfigurationImagesFolder");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+builder.Services.Configure<ConfigurationImages>(imageFolderConfig);
+
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+builder.Services.AddTransient<IFoodRepository, FoodRepository>();
+builder.Services.AddTransient<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+builder.Services.AddScoped<SalesReportService>();
+builder.Services.AddScoped<SalesChartService>();
+
+builder.Services.AddAuthorization(opt =>
+{
+    opt.AddPolicy("Admin", policy =>
+    {
+        policy.RequireRole("Admin");
+    });
+});
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped(sp => ShoppingCart.GetShoppingCart(sp));
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddPaging(opt =>
+{
+    opt.ViewName = "Bootstrap4";
+    opt.PageParameterName = "pageindex";
+});
+
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+CreateUserProfiles(app);
+
+app.UseSession();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
+
+    endpoints.MapControllerRoute(
+        name: "categoryFilter",
+        pattern: "Food/{action}/{category?}",
+        defaults: new { controller = "Food", action = "List" });
+
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
+
+static void CreateUserProfiles(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using var scope = scopedFactory.CreateScope();
+
+    var service = scope.ServiceProvider.GetService<ISeedUserRoleInitial>();
+
+    service.SeedUsers();
+    service.SeedRoles();
 }
